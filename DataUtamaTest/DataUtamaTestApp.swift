e630a352a48e57a65bbe3584e565393264dc0e17
Muhammad Abdul Ghani <ghani28muhammad@gmail.com>
2024-01-11 14:49:56 +0700

    Initial Commit

---
+++ DataUtamaTest/DataUtamaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataUtamaTestApp.swift
+//  DataUtamaTest
+//
+//  Created by Muhammad Abdul Ghani on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DataUtamaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
